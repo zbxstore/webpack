223f54954b799a5185257151783c8ceaf195064f
tanjie <tanie@foxmail.com>
2018-05-21 13:53:22 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
     mode: "production",
-    entry: './src/index.js',
+    entry: {
+        app:'./src/index.js',
+        print: './src/print.js'
+    },
+
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: '输出管理'
+        })
+
+    ],
+
     output:{
-        filename : "bundle.js",
+        filename : "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
     module:{
         rules:[
-            {
+            //加载css
+            {      
                 test: /\.css$/,
                 use:[
                     'style-loader',
                     'css-loader'
                 ]
             },
+            //加载图片
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     'file-loader'   
                 ]
-            }
+            },
+            //加载字体
         ]
     }
 }
